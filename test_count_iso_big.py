6690fb0a0224c710c7f0c12705067621c955067f
Paulo Oliva <92082845+Paulo-Oliva@users.noreply.github.com>
2023-04-06 13:58:37 +0200

    - Refactor the tests to reduce repetition - Fix some test cases in the fcr correctness tests

--- test_count_iso_big.py
+++ test_count_iso_big.py
@@ -1,139 +1,88 @@
 from graph_io import load_graph
-from count_isomorphisms_test import count_iso2
+
+from count_isomorphisms_test import count_iso
 import pytest
 
-base_path = "./graphs/"
+
+def _count_iso(name, expected_groups):
+    path = "./graphs/" + name
+    with open(path, "r", encoding="utf-8") as grl:
+        graphs, _ = load_graph(grl, read_list=True)
+    result = count_iso(graphs)
+
+    assert result == expected_groups
 
 
 @pytest.mark.timeout(80)
 def test_count_iso_products216():
-    path = base_path + "products216.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0,6],  1728),([1,7], 1728), ([2,9], 1728), ([3,8],10368),([4,5],1728)]
-    result = count_iso2(graphs)
-
-    assert result == expected_groups
+    _count_iso("products216.grl", [([0, 6], 1728), ([1, 7], 1728),
+                                   ([2, 9], 1728), ([3, 8], 10368),
+                                   ([4, 5], 1728)])
 
 
-    
 @pytest.mark.timeout(240)
 def test_count_iso_torus144():
-    path = base_path + "torus144.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0,6],  576),([1,7], 576), ([2,4], 576), ([3,10],576),([5,9],1152), ([8,11],576)]
-    result = count_iso2(graphs)
+    expected_groups = [([0, 6], 576), ([1, 7], 576), ([2, 4], 576),
+                       ([3, 10], 576), ([5, 9], 1152), ([8, 11], 576)]
+    _count_iso("torus144.grl", expected_groups)
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(240)
 def test_count_iso_cographs1():
-    path = base_path + "cographs1.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0,3],  5971968),([1,2], 995328)]
-    result = count_iso2(graphs)
+    _count_iso("cographs1.grl", [([0, 3], 5971968), ([1, 2], 995328)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(240)
 def test_count_iso_trees90():
-    path = base_path + "trees90.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0,3],  6912),([1,2], 20736)]
-    result = count_iso2(graphs)
+    _count_iso("trees90.grl", [([0, 3], 6912), ([1, 2], 20736)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(240)
 def test_count_iso_modulesC():
-    path = base_path + "modulesC.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0,7],  17915904),([1,5], 17915904), ([2,4], 2488320), ([3, 6], 2985984)]
-    result = count_iso2(graphs)
-
-    assert result == expected_groups
+    _count_iso("modulesC.grl", [([0, 7], 17915904), ([1, 5], 17915904),
+                                ([2, 4], 2488320), ([3, 6], 2985984)])
 
 
 @pytest.mark.timeout(80)
 def test_count_iso_cubes7():
-    path = base_path + "cubes7.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0,3],  645120),([1,2], 480)]
-    result = count_iso2(graphs)
+    _count_iso("cubes7.grl", [([0, 3], 645120), ([1, 2], 480)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_cubes9():
-    path = base_path + "cubes9.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 1], 185794560),([2, 3], 20160)]
-    result = count_iso2(graphs)
+    _count_iso("cubes9.grl", [([0, 1], 185794560), ([2, 3], 20160)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_bigtrees1():
-    path = base_path + "bigtrees1.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 2], 442368),([1, 3],  5308416)]
-    result = count_iso2(graphs)
+    _count_iso("bigtrees1.grl", [([0, 2], 442368), ([1, 3], 5308416)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_bigtrees2():
-    path = base_path + "bigtrees2.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 3], 80244904034304),([1, 2],  160489808068608)]
-    result = count_iso2(graphs)
+    _count_iso("bigtrees2.grl", [([0, 3], 80244904034304),
+                                 ([1, 2], 160489808068608)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_bigtrees3():
-    path = base_path + "bigtrees3.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 2], 2772351862699137701073289910157312),([1, 3],  462058643783189616845548318359552)]
-    result = count_iso2(graphs)
+    _count_iso("bigtrees3.grl", [([0, 2], 2772351862699137701073289910157312),
+                                 ([1, 3], 462058643783189616845548318359552)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_wheeljoin33():
-    path = base_path + "wheeljoin33.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 4], 8257536),([1, 2], 7962624),([3, 5], 50577408),([6, 7], 1290240)]
-    result = count_iso2(graphs)
+    _count_iso("wheeljoin33.grl", [([0, 4], 8257536), ([1, 2], 7962624),
+                                   ([3, 5], 50577408), ([6, 7], 1290240)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_wheelstar12():
-    path = base_path + "wheelstar12.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 3], 1935360),([1, 2], 6718464)]
-    result = count_iso2(graphs)
+    _count_iso("wheelstar12.grl", [([0, 3], 1935360), ([1, 2], 6718464)])
 
-    assert result == expected_groups
 
 @pytest.mark.timeout(80)
 def test_count_iso_wheelstar15():
-    path = base_path + "wheelstar15.grl"
-    with open(path, "r", encoding="utf-8") as grl:
-        graphs, _nothing = load_graph(grl, read_list=True)
-    expected_groups = [([0, 7], 1703116800),([1, 4] ,3009871872),([2, 3] ,10642046976),([5, 6], 2890137600)]
-    result = count_iso2(graphs)
-
-    assert result == expected_groups
+    _count_iso("wheelstar15.grl", [([0, 7], 1703116800), ([1, 4], 3009871872),
+                                   ([2, 3], 10642046976),
+                                   ([5, 6], 2890137600)])
--- test_preprocessing_correctness.py
+++ test_preprocessing_correctness.py
@@ -1,17 +1,18 @@
 from graph import Graph, Vertex, Edge
 from preprocessing import count_automorphisms, calc_true_twins, calc_false_twins, check_neighbourhood_TT, check_neighbourhood_FT
 
+
 def test_calc_TT_and_FT():
     G = Graph(False, 0)
     v1 = Vertex(G)
     v2 = Vertex(G)
     v3 = Vertex(G)
     v4 = Vertex(G)
-    e1 = Edge(v1,v2)
-    e2 = Edge(v1,v3)
-    e3 = Edge(v1,v4)
-    e4 = Edge(v2,v3)
-    e5 = Edge(v3,v4)
+    e1 = Edge(v1, v2)
+    e2 = Edge(v1, v3)
+    e3 = Edge(v1, v4)
+    e4 = Edge(v2, v3)
+    e5 = Edge(v3, v4)
     G.add_edge(e1)
     G.add_edge(e2)
     G.add_edge(e3)
@@ -19,10 +20,11 @@ def test_calc_TT_and_FT():
     G.add_edge(e5)
     TT = calc_true_twins(G)
     FT = calc_false_twins(G)
-    expected_TT = [[0,2]]
-    expected_FT = [[1,3]]
+    expected_TT = [[0, 2]]
+    expected_FT = [[1, 3]]
     assert expected_TT == TT
     assert expected_FT == FT
+
 
 def test_twin_false_twin():
     G = Graph(False, 0)
@@ -30,11 +32,11 @@ def test_twin_false_twin():
     v2 = Vertex(G)
     v3 = Vertex(G)
     v4 = Vertex(G)
-    e1 = Edge(v1,v2)
-    e2 = Edge(v1,v3)
-    e3 = Edge(v1,v4)
-    e4 = Edge(v2,v3)
-    e5 = Edge(v3,v4)
+    e1 = Edge(v1, v2)
+    e2 = Edge(v1, v3)
+    e3 = Edge(v1, v4)
+    e4 = Edge(v2, v3)
+    e5 = Edge(v3, v4)
     G.add_edge(e1)
     G.add_edge(e2)
     G.add_edge(e3)
@@ -45,12 +47,12 @@ def test_twin_false_twin():
     assert expected == num
 
 
-
 def test_single_vertex():
     G = Graph(False, 1)
     expected = 1
     num = count_automorphisms(G)
     assert expected == num
+
 
 def test_two_no_edges_vertex():
     G = Graph(False, 2)
@@ -58,40 +60,43 @@ def test_two_no_edges_vertex():
     num = count_automorphisms(G)
     assert expected == num
 
+
 def test_two_with_edge_vertex():
     G = Graph(False, 0)
     v1 = Vertex(G)
     v2 = Vertex(G)
-    e1 = Edge(v1,v2)
+    e1 = Edge(v1, v2)
     G.add_edge(e1)
     expected = 2
     num = count_automorphisms(G)
     assert expected == num
+
 
 def test_true_triplets_calc_TT():
     G = Graph(False, 0)
     v1 = Vertex(G)
     v2 = Vertex(G)
     v3 = Vertex(G)
-    e1 = Edge(v1,v2)
-    e2 = Edge(v1,v3)
-    e3 = Edge(v2,v3)
+    e1 = Edge(v1, v2)
+    e2 = Edge(v1, v3)
+    e3 = Edge(v2, v3)
     G.add_edge(e1)
     G.add_edge(e2)
     G.add_edge(e3)
 
     result = calc_true_twins(G)
-    expected = [[0,1,2]]
+    expected = [[0, 1, 2]]
     assert expected == result
+
 
 def test_true_triplets():
     G = Graph(False, 0)
     v1 = Vertex(G)
     v2 = Vertex(G)
     v3 = Vertex(G)
-    e1 = Edge(v1,v2)
-    e2 = Edge(v1,v3)
-    e3 = Edge(v2,v3)
+    e1 = Edge(v1, v2)
+    e2 = Edge(v1, v3)
+    e3 = Edge(v2, v3)
     G.add_edge(e1)
     G.add_edge(e2)
     G.add_edge(e3)
