e938c24e258cefff312fd39a1074488173bb84ac
Paulo Oliva <92082845+Paulo-Oliva@users.noreply.github.com>
2023-04-17 20:23:42 +0200

    Refactor doubly_linked_list

--- src/doubly_linked_list.py
+++ src/doubly_linked_list.py
@@ -1,7 +1,24 @@
+"""
+This module contains the implementation of a doubly linked list.
+
+Classes:
+    - Node: A node of a doubly linked list.
+    - DoublyLinkedList: A doubly linked list implementation for the vertices
+        of a graph.
+"""
+
 from .graph import Vertex
 
 
 class Node:
+    """
+    A node of a doubly linked list.
+
+    Attributes:
+        data: The data of the node.
+        next: The next node in the list.
+        prev: The previous node in the list.
+    """
 
     def __init__(self, data: Vertex):
         self.data = data
@@ -16,14 +33,35 @@ class Node:
 
 
 class DoublyLinkedList:
+    """
+    A doubly linked list implementation for the vertices of a graph.
+
+    Attributes:
+        head: The head of the list.
+        tail: The tail of the list.
+        size: The size of the list.
+    """
 
     def __init__(self):
         self.head: Node | None = None
         self.tail: Node | None = None
         self.size = 0
 
-    def append_vertex(self, data: Vertex):
+    def append_vertex(self, data: Vertex) -> Node:
+        """
+        Append a vertex to the list.
+
+        This method also returns the node containing the vertex that was
+        appended.
+
+        Args:
+            data (Vertex): The vertex to be appended.
+
+        Returns:
+            Node: The node containing the vertex that was appended.
+        """
         new_node = Node(data)
+
         if self.head is None:
             self.head = new_node
             self.tail = new_node
@@ -37,6 +75,12 @@ class DoublyLinkedList:
         return new_node
 
     def append_node(self, node: Node):
+        """
+        Append a node to the list.
+
+        Args:
+            node (Node): The node to be appended.
+        """
         if self.head is None:
             self.head = node
             self.tail = node
@@ -48,6 +92,14 @@ class DoublyLinkedList:
         self.size += 1
 
     def remove(self, node: Node):
+        """
+        Remove a node from the list.
+
+        The removed node also has its prev and next attributes set to None.
+
+        Args:
+            node (Node): The node to be removed.
+        """
         if node.prev is None:
             self.head = node.next
         else:
@@ -62,9 +114,6 @@ class DoublyLinkedList:
         node.next = None
 
         self.size -= 1
-
-    def is_empty(self):
-        return self.head is None
 
     def __iter__(self):
         node = self.head
