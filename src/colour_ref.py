608d6a3747a995b8406b4a8ef873d6324d7c1092
Paulo Oliva <92082845+Paulo-Oliva@users.noreply.github.com>
2023-04-17 17:20:08 +0200

    Refactor colour_ref.py

--- src/colour_ref.py
+++ src/colour_ref.py
@@ -1,6 +1,11 @@
 """
-This module contains an implementation of the colour refinement algorithm
-and a function to check if two graphs are isomorphic.
+This module contains implementations of the colour refinement algorithm.
+
+Functions:
+- colour_refinement_normal: Refines the colouring of a graph using the
+    standard colour refinement algorithm.
+- colour_refinement: Refines the colouring of a graph using the
+    fast colour refinement algorithm.
 """
 
 from collections import deque
@@ -9,28 +14,20 @@ from .doubly_linked_list import DoublyLinkedList, Node
 from .graph import Graph, Vertex
 
 
-def create_colours_to_vertices_dict(
-        colouring: dict[Vertex, int]) -> dict[int, set[Vertex]]:
-    colours_to_vertices_dict = {}
-    for v, col in colouring.items():
-        if col not in colours_to_vertices_dict.keys():
-            colours_to_vertices_dict[col] = {v}
-        else:
-            colours_to_vertices_dict[col].add(v)
-    return colours_to_vertices_dict
-
-
-def colour_refinement_old(vertices: list[Vertex],
-                          colouring: dict[Vertex, int]) -> dict[Vertex, int]:
+def colour_refinement_normal(
+        vertices: list[Vertex], colouring: dict[Vertex,
+                                                int]) -> dict[Vertex, int]:
     """
-    This function implements the colour refinement algorithm described in`
+    Refine the colouring of a graph using the standard colour refinement.
+
+    This function implements the colour refinement algorithm described in
     the slides of the lecture. The main difference is that this function
-    takes a list of vertices instead of a graph, instead of the graph itself.
-    This allows us to run the algorithm on two graphs at the same time more
-    easily, and thus, check if they are isomorphic.
+    takes a list of vertices instead of the graph itself.
 
     Args:
         vertices (list[Vertex]): A list of the vertices of the graph(s).
+        colouring (dict[Vertex, int]): A dictionary mapping each vertex to a
+            colour represented by a number.
 
     Returns:
         dict[Vertex, int]: A dictionary mapping each vertex to a colour
@@ -38,15 +35,14 @@ def colour_refinement_old(vertices: list[Vertex],
     """
     while True:
         colour = 0
-        # Create a new dictionary to store the colours and add all the vertices to it
+        # Create a copy of the colouring
         new_colouring = colouring.copy()
-        # This dictionary maps the colours of the neighbours of a vertex
-        # to a new colour
+        # Dictionary that maps a tuple of colours to a colour
         neighbours_to_colour = {}
 
-        for v in vertices:
+        for vertex in vertices:
             # Get the neighbours of the current vertex
-            neighbours = v.neighbours
+            neighbours = vertex.neighbours
             # Get the colours of the neighbours as a sorted tuple
             neighbour_colours = tuple(
                 sorted([colouring[n] for n in neighbours]))
@@ -56,7 +52,7 @@ def colour_refinement_old(vertices: list[Vertex],
                 neighbours_to_colour[neighbour_colours] = colour
                 colour += 1
             # Assign the correct colour to the current vertex
-            new_colouring[v] = neighbours_to_colour[neighbour_colours]
+            new_colouring[vertex] = neighbours_to_colour[neighbour_colours]
 
         # Check if the colouring has changed
         if new_colouring == colouring:
@@ -70,10 +66,10 @@ def colour_refinement_old(vertices: list[Vertex],
 
 def are_isomorphic(g1: Graph, g2: Graph) -> tuple[bool, bool]:
     """
-    This function checks if two graphs are isomorphic.
-    To do so, it checks if the vertices, edges and degrees are preserved
-    between the two graphs checks if there's a bijection between the
-    colourings of the two graphs using the colour refinement algorithm.
+    Check if two graphs are isomorphic.
+
+    This function is no longer used in the project. It is kept here for
+    proof of work from the first assignment.
 
     Args:
         g1 (Graph): The first graph.
@@ -93,7 +89,7 @@ def are_isomorphic(g1: Graph, g2: Graph) -> tuple[bool, bool]:
     vertices = g1.vertices + g2.vertices
 
     alpha = {v: v.degree for v in vertices}
-    colouring = colour_refinement(vertices, alpha)
+    colouring = colour_refinement_normal(vertices, alpha)
 
     # Separate the colouring into two colourings, one for each graph
     colouring_g1 = {v: colouring[v] for v in g1.vertices}
@@ -114,7 +110,7 @@ def are_isomorphic(g1: Graph, g2: Graph) -> tuple[bool, bool]:
 
 def is_discrete_colouring(colouring: dict[Vertex, int]) -> bool:
     """
-    This function checks if a colouring is a discrete.
+    Check if a colouring is a discrete.
 
     Args:
         colouring (dict[Vertex, int]): A dictionary mapping each vertex to a
@@ -123,88 +119,116 @@ def is_discrete_colouring(colouring: dict[Vertex, int]) -> bool:
     Returns:
         bool: A boolean indicating if the colouring is discrete.
     """
-    # Get the number of colours and vertices in the colouring
     colour_amount = len(set(colouring.values()))
     vertex_amount = len(colouring.keys())
-    # Return whether the number of colours is equal to the number of vertices
+    # The colouring is discrete if the number of colours is equal to the
+    # number of vertices
     return colour_amount == vertex_amount
 
 
+# TODO: Remove the unused argument in the future
 def colour_refinement(_, colouring: dict[Vertex, int]) -> dict[Vertex, int]:
+    """
+    Refine the colouring of a graph using fast colour refinement.
 
-    colour_classes, vertex_to_node = create_colour_and_vertex_dicts(colouring)
+    Args:
+        _ : Unused argument, only here to preserve the same function signature
+            as the old colour refinement function.
+        colouring (dict[Vertex, int]): A dictionary mapping each vertex of the
+            graph to a colour represented by a number. In a initial case, this
+            colouring should be based on the degree of the vertices.
 
+    Returns:
+        dict[Vertex, int]: The refined colouring of the graph, based on the
+            colouring given as input.
+    """
+    colour_classes, vertex_to_node = _create_colour_and_vertex_dicts(colouring)
+
+    # We cannot refine the colouring if there is only one colour class
     if len(colour_classes) == 1:
         return colouring
 
     queue = deque()
     # Append all colour classes to the queue in decreasing order of size
-    for c in sorted(colour_classes,
-                    key=lambda c: len(colour_classes[c]),
-                    reverse=True):
-        queue.append(c)
+    for colour in sorted(colour_classes,
+                         key=lambda c: len(colour_classes[c]),
+                         reverse=True):
+        queue.append(colour)
 
     in_queue = {c: True for c in colour_classes}
 
-    l = max(colour_classes.keys())
+    new_colour = max(colour_classes.keys())
 
     while queue:
         # Pop the first colour class from the queue
-        C = queue.popleft()
-        in_queue[C] = False
-        # print("Popped colour class", C, "from queue")
+        c = queue.popleft()
+        in_queue[c] = False
 
         # Compute the adjacent colour classes
         L = set()
         A = {}
-        for v in colour_classes[C]:
-            neighbours = v.neighbours
+        for vertex in colour_classes[c]:
+            neighbours = vertex.neighbours
             for n in neighbours:
                 colour_of_n = colouring[n]
                 L.add(colour_of_n)
-                if A.get(colour_of_n) == None:
+                if A.get(colour_of_n) is None:
                     A[colour_of_n] = {n}
                 else:
                     A[colour_of_n].add(n)
-
+        # For each adjacent colour class, check if it should be split
         for i in L:
             if i < 0:
                 continue
             if len(A[i]) < len(colour_classes[i]):
-                l += 1
-                colour_classes[l] = DoublyLinkedList()
-                # print("Splitting up colour class", i, "into", i, "and", l)
-
-                for v in A[i]:
-                    node = vertex_to_node[v]
+                # Create a new colour class
+                new_colour += 1
+                colour_classes[new_colour] = DoublyLinkedList()
+                # Move the vertices from the old colour class to the new one
+                for vertex in A[i]:
+                    node = vertex_to_node[vertex]
                     colour_classes[i].remove(node)
-                    colour_classes[l].append_node(node)
-                    colouring[v] = l
-
-                # If i in Queue: add l to the queue.
+                    colour_classes[new_colour].append_node(node)
+                    colouring[vertex] = new_colour
+                # Update the queue
                 if in_queue[i]:
-                    queue.append(l)
-                    in_queue[l] = True
-                # If i not in Queue: add either i or l to the queue, depending on which of the new color classes is the smallest.
+                    queue.append(new_colour)
+                    in_queue[new_colour] = True
                 else:
-                    if len(colour_classes[i]) < len(colour_classes[l]):
+                    if len(colour_classes[i]) < len(
+                            colour_classes[new_colour]):
                         queue.append(i)
                         in_queue[i] = True
-                        in_queue[l] = False
+                        in_queue[new_colour] = False
                     else:
-                        queue.append(l)
-                        in_queue[l] = True
+                        queue.append(new_colour)
+                        in_queue[new_colour] = True
     return colouring
 
 
-def create_colour_and_vertex_dicts(
+def _create_colour_and_vertex_dicts(
     colouring: dict[Vertex, int]
 ) -> tuple[dict[int, DoublyLinkedList], dict[Vertex, Node]]:
+    """
+    Create 2 dictionaries for further use in the fast colour ref. algorithm.
+
+    The 1st dictionary maps each colour to a doubly linked list of vertices
+    with that colour. The 2nd dictionary maps each vertex to the node in the
+    doubly linked list that contains that vertex.
+
+    Args:
+        colouring (dict[Vertex, int]): The colouring of the graph.
+
+    Returns:
+        tuple[dict[int, DoublyLinkedList], dict[Vertex, Node]]: A tuple
+            containing the 2 dictionaries.
+    """
     colour_classes: dict[int, DoublyLinkedList] = {}
     vertex_to_node: dict[Vertex, Node] = {}
-    for v, c in colouring.items():
-        if c not in colour_classes:
-            colour_classes[c] = DoublyLinkedList()
-        n = colour_classes[c].append_vertex(v)
-        vertex_to_node[v] = n
+
+    for vertex, colour in colouring.items():
+        if colour not in colour_classes:
+            colour_classes[colour] = DoublyLinkedList()
+        node = colour_classes[colour].append_vertex(vertex)
+        vertex_to_node[vertex] = node
     return colour_classes, vertex_to_node
